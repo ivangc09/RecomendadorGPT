b00dce7fbc47d7b2ed8c0e6342bd1fb83df16a67
ivangc09 <garcia.cruz.ivan09@gmail.com>
2025-10-05 14:10:29 -0600

    Ajustes uvicorn

--- main.py
+++ main.py
@@ -116,4 +116,5 @@ async def recomendar(req: RecoRequest, request: Request):
 # ---- main ----
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
